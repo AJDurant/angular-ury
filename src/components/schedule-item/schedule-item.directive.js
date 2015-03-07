08fc4a2d9286870acb2163c4c692d989f89956f5
Andy Durant <andy@ajdurant.co.uk>
2015-03-07 13:50:15 +0000

    element query & duration height

--- src/components/schedule-item/schedule-item.directive.js
+++ src/components/schedule-item/schedule-item.directive.js
@@ -7,6 +7,11 @@ angular.module('ury')
             scope: {
                 item: '=show'
             },
+            link: function(scope, element) {
+                element.css({
+                    height: 60 * scope.item.duration + 'px'
+                });
+            },
             templateUrl: 'components/schedule-item/schedule-item.html'
         };
     });
