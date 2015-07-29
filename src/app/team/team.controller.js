ee5c397b1d2e3456cf75944db54f0708a8cde3cb
Andy Durant <andy@ajdurant.co.uk>
2015-07-29 16:10:05 +0100

    team api update

--- src/app/team/team.controller.js
+++ src/app/team/team.controller.js
@@ -4,26 +4,14 @@ angular.module('ury')
     .controller('TeamCtrl', ['$scope', '$routeParams', 'uryAPI',
         function ($scope, $routeParams, uryAPI) {
 
-            var teams = {
-                presenting: 16,
-                production: 3,
-                news: 5,
-                speech: 6,
-                marketing: 15,
-                music: 4,
-                engineering: 7,
-                computing: 8,
-                events: 14
-            };
-
             var team = $routeParams.team;
-            var teamID = teams[team];
 
             uryAPI().get(
                 {
                     module: 'team',
-                    ID: teamID,
-                    full: true
+                    method: 'byalias',
+                    firstParam: team,
+                    mixins: 'officers'
                 },
                 function (data) {
                     $scope.team = data.payload;
--- src/components/services/api.factory.js
+++ src/components/services/api.factory.js
@@ -3,16 +3,17 @@
 angular.module('ury')
     .factory('uryAPI', ['$resource', function($resource) {
 
-        var apiURL = 'https://ury.org.uk/api/v2';
+        var apiURL = 'https://ury.org.uk/api-aj/v2';
         var apiKey = '00687b83-73b2-43ee-8083-fcbd7811def6';
 
         return function(/*, options*/){
+            // get all extra paramerters passed in - these are used for the data object sent with the request
             var options = arguments[0] || {};
             options.api_key = apiKey;
 
             return $resource(
-                apiURL + '/:module/:ID/:method',
-                options
+                apiURL + '/:module/:ID/:method/:firstParam', // specified params are in the URL
+                options // all other options are passed as data to the API
             );
 
         };
