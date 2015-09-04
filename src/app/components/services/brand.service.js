286c0b86d36690e9c7ee9bf0ca49171db66209b8
Andy Durant <andy@ajdurant.co.uk>
2015-09-05 00:01:56 +0100

    update brands for this year

--- src/app/components/services/brand.service.js
+++ src/app/components/services/brand.service.js
@@ -6,10 +6,10 @@
         .service('uryBrand', uryBrand);
 
     function uryBrand () {
-        var flagship = /^URY\s?(Breakfast|Lunch|:PM)/i;
-        var news = /^(URY (News|Sports|Football|Election)|Candidate Interview Night|Election Results Night|YUSU Election)/i;
+        var flagship = /^URY\s?(Breakfast|Brunch|Lunch|:PM)/i;
+        var news = /^(URY (News|Sports|Football|Election)|Candidate Interview Night|Election Results Night|YUSU Election|York Sport Report)/i;
         var speech = /^(URY Speech|YorWorld|In the Stalls|Screen|Stage|Game Breaking|Radio Drama)/i;
-        var events = /(Roses (Live )?\d{4}|Woodstock)/i;
+        var events = /(Roses (Live )?\d{4}|Woodstock|Fresher'?s)/i;
 
         this.getBrand = function (title, time) {
             if (flagship.test(title)) {
