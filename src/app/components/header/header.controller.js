ca62a5b456a02a9451b54ff8ce6c03555738d741
Andy Durant <andy@ajdurant.co.uk>
2015-09-04 23:48:39 +0100

    update every 3 mins

--- src/app/components/header/header.controller.js
+++ src/app/components/header/header.controller.js
@@ -6,19 +6,26 @@
         .controller('HeaderCtrl', HeaderCtrl);
 
     /** @ngInject */
-    function HeaderCtrl ($scope, uryAPI, uryStatus) {
+    function HeaderCtrl ($scope, $interval, uryAPI, uryStatus) {
 
         $scope.uryStatus = uryStatus;
 
-        uryAPI('get', {
-                module: 'timeslot',
-                method: 'currentandnext'
-            }
-        ).then(function(data) {
-            $scope.show = data.payload;
-            // Update global status
-            $scope.uryStatus.onAir = (typeof $scope.show.current.url !== 'undefined');
-            $scope.uryStatus.timeslot = (typeof $scope.show.current.id !== 'undefined') ? $scope.show.current.id : null;
-        });
+        var update = function () {
+            uryAPI('get', {
+                    module: 'timeslot',
+                    method: 'currentandnext'
+                }
+            ).then(function(data) {
+                $scope.show = data.payload;
+                // Update global status
+                $scope.uryStatus.onAir = (typeof $scope.show.current.url !== 'undefined');
+                $scope.uryStatus.timeslot = (typeof $scope.show.current.id !== 'undefined') ? $scope.show.current.id : null;
+            });
+        };
+
+        // update the status immediately, then every 3 mins
+        update();
+        $interval(update, 180000);
+
     }
 })();
